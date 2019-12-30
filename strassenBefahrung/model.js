ebf227e6e61c4561ad5fcbda3777dc8877e59d4e
M-JW <itm.weiter@muenchen.de>
2019-12-30 09:23:28 +0100

    update strasssenbefahrung user places marker in map

--- strassenBefahrung/model.js
+++ strassenBefahrung/model.js
@@ -1,14 +1,45 @@
 import Tool from "../../modules/core/modelList/tool/model";
+import {Pointer} from "ol/interaction.js";
 
 const StrassenBefahrungModel = Tool.extend({
     defaults: Object.assign({}, Tool.prototype.defaults, {
         id: "strassenBefahrung",
         name: "Straßen-Befahrung",
         glyphicon: "glyphicon-pencil",
-        renderToWindow: false
+        renderToWindow: false,
+        interaction: null
     }),
     initialize: function () {
         this.superInitialize();
+    },
+    placeMarkerInMap: function () {
+        const interaction = new Pointer({
+            handleMoveEvent: function (evt) {
+                this.showMarker(evt.coordinate);
+            }.bind(this),
+            handleDownEvent: function (evt) {
+                this.setMarker(evt.coordinate);
+            }.bind(this)
+        }, this);
+
+        this.setInteraction(interaction);
+
+        Radio.trigger("Map", "addInteraction", this.get("interaction"));
+    },
+    showMarker: function (coord) {
+        Radio.trigger("MapMarker", "showMarker", coord);
+    },
+    setMarker: function (coord) {
+        this.showMarker(coord);
+        this.removeInteraction();
+        this.trigger("initInfra3d", coord);
+    },
+    removeInteraction: function () {
+        Radio.trigger("Map", "removeInteraction", this.get("interaction"));
+        this.setInteraction(null);
+    },
+    setInteraction: function (value) {
+        this.set("interaction", value);
     }
 });
 
--- strassenBefahrung/view.js
+++ strassenBefahrung/view.js
@@ -6,11 +6,12 @@ const StrassenBefahrungView = Backbone.View.extend({
     events: {
         "click .close": "hide",
         "click .btn-nav": "toggleNavigation",
-        "click .btn-listen": "startPositionChangedListener"
+        "click .btn-marker": "toggleMarker"
     },
     initialize: function () {
         this.listenTo(this.model, {
-            "change:isActive": this.render
+            "change:isActive": this.render,
+            "initInfra3d": this.initInfra3d
         });
 
         if (this.model.get("isActive") === true) {
@@ -27,7 +28,8 @@ const StrassenBefahrungView = Backbone.View.extend({
             this.$el.html(this.template(attr));
             Radio.trigger("Sidebar", "append", this.el);
             Radio.trigger("Sidebar", "toggle", true, "50%");
-            this.initInfra3d();
+            this.addPostMessageListenser();
+            // this.initInfra3d();
         }
         else {
             Radio.trigger("Sidebar", "toggle", false);
@@ -39,16 +41,21 @@ const StrassenBefahrungView = Backbone.View.extend({
         this.model.setIsActive(false);
     },
 
-    initInfra3d: function () {
+    initInfra3d: function (coord) {
         const infra3d = window.infra3d,
             divId = "infra3d-div",
             url = "https://client-v3.infra3d.ch",
+            epsgCodeString = Radio.request("MapView", "getProjection").getCode(),
+            epsgCodeNumber = parseInt(epsgCodeString.split(":")[1], 10),
             options = {
                 loginurl: "https://auth.infra3d.ch/api/v1/login",
                 credentials: ["WOmuenchen", "6tHqJ2"],
-                easting: 11.571,
-                northing: 48.132,
-                epsg: 4326,
+                // easting: 11.571,
+                // northing: 48.132,
+                // epsg: 4326,
+                easting: coord[0],
+                northing: coord[1],
+                epsg: epsgCodeNumber,
                 lang: "de",
                 map: false,
                 layer: false,
@@ -60,35 +67,65 @@ const StrassenBefahrungView = Backbone.View.extend({
         if (infra3d) {
             infra3d.init(divId, url, options, function () {
                 console.error("Infra3D initialized");
-            }, null);
+                this.setOnPositionChanged();
+            }, this);
         }
         else {
             console.error("Infra3D is not loaded!");
         }
+        this.deactivateButton(".btn-marker");
     },
-    startPositionChangedListener: function () {
-        const infra3d = window.infra3d;
+    setOnPositionChanged: function () {
+        console.error("setOnPositionChanged");
 
-        infra3d.setOnPositionChanged(function (easting) {
-            console.error(easting);
+        window.infra3d.setOnPositionChanged(function () {
+            console.error("position changed!!!");
         }, null);
+    },
+    addPostMessageListenser: function () {
+        window.addEventListener("message", function (evt) {
+            console.error(evt);
+        });
     },
     toggleNavigation: function () {
         const btnNav = this.$el.find(".btn-nav"),
             hasClass = btnNav.hasClass("btn-primary");
 
         if (hasClass) {
+            this.deactivateButton(".btn-nav");
             window.infra3d.setNavigationVisibility(false);
             // window.infra3d.iframe.contentWindow.postMessage("setNavigationVisibility(false)", window.infra3d.origin);
-            btnNav.removeClass("btn-primary");
-            btnNav.addClass("btn-default");
         }
         else {
+            this.activateButton(".btn-nav");
             window.infra3d.setNavigationVisibility(true);
             // window.infra3d.iframe.contentWindow.postMessage("setNavigationVisibility(true)", window.infra3d.origin);
-            btnNav.removeClass("btn-default");
-            btnNav.addClass("btn-primary");
         }
+    },
+    toggleMarker: function () {
+        const btnNav = this.$el.find(".btn-marker"),
+            hasClass = btnNav.hasClass("btn-primary");
+
+        if (hasClass) {
+            this.deactivateButton(".btn-marker");
+        }
+        else {
+            this.activateButton(".btn-marker");
+            this.model.placeMarkerInMap();
+        }
+    },
+    activateButton: function (className) {
+        const btn = this.$el.find(className);
+
+        btn.removeClass("btn-default");
+        btn.addClass("btn-primary");
+
+    },
+    deactivateButton: function (className) {
+        const btn = this.$el.find(className);
+
+        btn.removeClass("btn-primary");
+        btn.addClass("btn-default");
     }
 });
 
