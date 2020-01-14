37f0a9eebf2d1532a006fa0fc2632edd3dddaab7
M-JW <itm.weiter@muenchen.de>
2020-01-14 13:41:00 +0100

    new addon performanceTester

---
+++ performanceTester/model.js
@@ -0,0 +1,113 @@
+import Feature from "ol/Feature.js";
+import Point from "ol/geom/Point.js";
+/**
+ * TODO
+ * @returns {Object} The model.
+ */
+function initializePerformanceTesterModel () {
+    const PerformanceTesterModel = Radio.request("ModelList", "getModelByAttributes", {id: "performanceTester"}),
+        defaults = {
+            name: "performanceTester",
+            glyphicon: "glyphicon-dashboard",
+            numFeatures: 1000,
+            extent: [670000, 5320000, 710000, 5350000],
+            height: 600,
+            intervalTime: 2000,
+            movement: false
+        };
+
+    Object.assign(PerformanceTesterModel, {
+        attributes: Object.assign(defaults, PerformanceTesterModel.attributes),
+        initialize: function () {
+            const layer = Radio.request("Map", "createLayerIfNotExists", "performanceTest");
+
+            this.superInitialize();
+            this.setLayer(layer);
+        },
+
+        createFeatures: function () {
+            const source = this.get("layer").getSource(),
+                numFeatures = this.get("numFeatures");
+            let feature;
+
+            this.deleteFeatures();
+            for (let i = 0; i < numFeatures; i++) {
+                feature = this.createRandomFeature();
+                source.addFeature(feature);
+            }
+            if (this.get("movement")) {
+                this.startMovement(source.getFeatures());
+            }
+        },
+
+        startMovement: function (features) {
+            const intervalId = setInterval(this.moveFeatures, this.get("intervalTime"), features, this);
+
+            this.setIntervalId(intervalId);
+        },
+
+        moveFeatures: function (features, context) {
+            features.forEach(feature => {
+                const newRandomCoords = context.createRandomCoordinate();
+
+                feature.getGeometry().setCoordinates(newRandomCoords);
+            });
+        },
+        createRandomFeature: function () {
+            const coordinate = this.createRandomCoordinate(),
+                feature = new Feature({
+                    geometry: new Point(coordinate)
+                });
+
+            return feature;
+        },
+
+        createRandomCoordinate: function () {
+            const extent = this.get("extent"),
+                minX = extent[0],
+                minY = extent[1],
+                maxX = extent[2],
+                maxY = extent[3],
+                xValue = this.createRandomValue(minX, maxX),
+                yValue = this.createRandomValue(minY, maxY),
+                height = this.get("height"),
+                randomCoordinate = [xValue, yValue, height];
+
+            return randomCoordinate;
+        },
+
+        createRandomValue: function (min, max) {
+            const randomValue = Math.random() * (max - min) + min;
+
+            return randomValue;
+        },
+        deleteFeatures: function () {
+            window.clearInterval(this.get("intervalId"));
+            this.get("layer").getSource().clear();
+        },
+
+        setLayer: function (value) {
+            this.set("layer", value);
+        },
+
+        setNumFeatures: function (value) {
+            this.set("numFeatures", value);
+        },
+
+        setHeight: function (value) {
+            this.set("height", value);
+        },
+
+        setMovement: function (value) {
+            this.set("movement", value);
+        },
+
+        setIntervalId: function (value) {
+            this.set("intervalId", value);
+        }
+    });
+
+    PerformanceTesterModel.initialize();
+    return PerformanceTesterModel;
+}
+export default initializePerformanceTesterModel;
---
+++ performanceTester/view.js
@@ -0,0 +1,75 @@
+import PerformanceTesterTemplate from "text-loader!./template.html";
+import PerformanceTesterModel from "./model";
+import "./style.less";
+
+const PerformanceTesterView = Backbone.View.extend(/** @lends PerformanceTesterView.prototype */{
+    events: {
+        "change #input-num-features": "setNumFeatures",
+        "change #input-height": "setHeight",
+        "click .create-features": "createFeatures",
+        "click .delete-features": "deleteFeatures",
+        "click .btn-movement-yes": "activateMovement",
+        "click .btn-movement-no": "deactivateMovement"
+    },
+    initialize: function () {
+        this.listenTo(this.model, {
+            "change:isActive": this.render
+        });
+
+        if (this.model.get("isActive") === true) {
+            this.render(this.model, this.model.get("isActive"));
+        }
+    },
+    model: new PerformanceTesterModel(),
+    template: _.template(PerformanceTesterTemplate),
+
+    render: function (model, isActive) {
+        if (isActive) {
+            this.setElement(document.getElementsByClassName("win-body")[0]);
+            this.$el.html(this.template(model.toJSON()));
+            this.delegateEvents();
+        }
+        else {
+            this.undelegateEvents();
+        }
+        return this;
+    },
+    setNumFeatures: function (evt) {
+        const numFeatures = parseInt(evt.target.value, 10);
+
+        this.model.setNumFeatures(numFeatures);
+    },
+    setHeight: function (evt) {
+        const height = parseInt(evt.target.value, 10);
+
+        this.model.setHeight(height);
+    },
+    createFeatures: function () {
+        this.model.createFeatures();
+    },
+    deleteFeatures: function () {
+        this.model.deleteFeatures();
+    },
+    activateMovement: function () {
+        this.activateButton(".btn-movement-yes");
+        this.deactivateButton(".btn-movement-no");
+        this.model.setMovement(true);
+        this.$el.find("#input-interval-time").removeClass("hide");
+    },
+    deactivateMovement: function () {
+        this.deactivateButton(".btn-movement-yes");
+        this.activateButton(".btn-movement-no");
+        this.model.setMovement(false);
+        this.$el.find("#input-interval-time").addClass("hide");
+    },
+    activateButton: function (selector) {
+        this.$el.find(selector).removeClass("btn-default");
+        this.$el.find(selector).addClass("btn-primary");
+    },
+    deactivateButton: function (selector) {
+        this.$el.find(selector).removeClass("btn-primary");
+        this.$el.find(selector).addClass("btn-default");
+    }
+});
+
+export default PerformanceTesterView;
