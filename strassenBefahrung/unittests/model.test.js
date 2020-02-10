6fc7f8a0537b1c05ef943b8674296aac93ceb9da
M-JW <itm.weiter@muenchen.de>
2020-02-10 08:34:22 +0100

    prepared unittests for straßenbefahrung

--- strassenBefahrung/unittests/model.test.js
+++ strassenBefahrung/unittests/model.test.js
@@ -1,26 +1,30 @@
 import {expect} from "chai";
 import ModelList from "@modules/core/modelList/list.js";
-import Model from "@addons/strassenBefahrung/model.js";
+import Tool from "@modules/core/modelList/tool/model.js";
+import initializeStrassenBefahrungModel from "@addons/strassenBefahrung/model.js";
 
-let modelList,
+let tool,
     model;
 
 before(function () {
-    modelList = new ModelList([{
+    tool = new Tool({
         name: "Straßen-Befahrung",
         glyphicon: "glyphicon-road",
         styleId: "strassenBefahrung_woman",
         type: "tool",
         parentId: "tools",
         id: "strassenBefahrung"
-    }]);
-    model = new Model();
+    });
+    new ModelList([tool]);
+    model = initializeStrassenBefahrungModel();
 });
 
 describe("addons/strassenBefahrung", function () {
-    describe("foobar", function () {
+    describe("setCoords", function () {
         it("BARFOO", function () {
-            expect(true).to.be.true;
+            model.setCoords([1, 2]);
+
+            expect(model.get("coords")).to.deep.equal([1, 2]);
         });
     });
 });
