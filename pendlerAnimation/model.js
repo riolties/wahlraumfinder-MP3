5d05bc3da10ba27878b4f78434e66261ca086d42
M-JW <itm.weiter@muenchen.de>
2020-03-17 16:45:26 +0100

    pendler

--- pendlerAnimation/model.js
+++ pendlerAnimation/model.js
@@ -1,3 +1,4 @@
+import {GeoJSON} from "ol/format.js";
 /**
  * TODO
  * @returns {Object} The model.
@@ -5,14 +6,102 @@
 function initializePendlerAnimationModel () {
     const PendlerAnimationModel = Radio.request("ModelList", "getModelByAttributes", {id: "pendlerAnimation"}),
         defaults = {
+            attributionText: "&copy; <a href='https://statistik.arbeitsagentur.de/' target='_blank'>Statistik der Bundesagentur für Arbeit</a>",
+            dataType: "GeoJSON",
+            url: "",
+            classes: [],
+            selectedClass: {},
+            currentLevel: 0
         };
 
     Object.assign(PendlerAnimationModel, {
         attributes: Object.assign(defaults, PendlerAnimationModel.attributes),
         initialize: function () {
-            this.superInitialize();
-            console.log(123);
+            const dataType = this.get("dataType"),
+                url = Radio.request("Util", "getProxyURL", this.get("url"));
 
+            this.listenTo(this, {
+                "change:isActive": function (model, value) {
+                    if (value) {
+                        Radio.trigger("Attributions", "createAttribution", model.get("name"), this.get("attributionText"), "Pendler");
+                    }
+                    else {
+                        Radio.trigger("Attributions", "removeAttribution", model.get("name"), this.get("attributionText"), "Pendler");
+                    }
+                }
+            });
+            this.superInitialize();
+            this.loadData(dataType, url);
+        },
+
+        loadData: function (dataType, url) {
+            let features = [];
+
+            if (dataType === "GeoJSON") {
+                features = this.loadDataFromGeoJson(url);
+            }
+            this.setFeatures(features);
+        },
+
+        loadDataFromGeoJson: function (url) {
+            const xhr = new XMLHttpRequest(),
+                format = new GeoJSON();
+            let features = [];
+
+            xhr.open("GET", url, false);
+            xhr.onreadystatechange = function (evt) {
+                const responseText = evt.currentTarget.responseText;
+
+                features = format.readFeatures(JSON.parse(responseText));
+            };
+            xhr.send();
+            return features;
+        },
+        selectClass: function (className) {
+            const features = this.get("features"),
+                selectedClass = this.get("classes").filter(classPart => {
+                    return classPart.name === className;
+                })[0];
+
+            selectedClass.levels[0].values = this.getFeatureValuesByLevel(features, selectedClass.levels[0]);
+            this.setSelectedClass(selectedClass);
+            this.setCurrentLevel(0);
+            this.render();
+        },
+
+        getFeatureValuesByLevel: function (features, level) {
+            let values = [];
+
+            features.forEach(feature => {
+                values.push(feature.get(level.attr));
+            });
+
+            values = [...new Set(values)];
+            values = values.sort();
+            return values;
+        },
+        selectLevel: function (level, selection) {
+            console.log(level);
+            console.log(selection);
+            const features = this.get("features"),
+                selectedClass = this.get("selectedClass");
+
+            selectedClass.levels[level].values = this.getFeatureValuesByLevel(features, selectedClass.levels[level]);
+            this.setSelectedClass(selectedClass);
+            this.setCurrentLevel(level);
+            this.render();
+        },
+        render: function () {
+            this.trigger("render", this, true);
+        },
+        setFeatures: function (value) {
+            this.set("features", value);
+        },
+        setSelectedClass: function (value) {
+            this.set("selectedClass", value);
+        },
+        setCurrentLevel: function (value) {
+            this.set("currentLevel", value);
         }
     });
 
--- pendlerAnimation/view.js
+++ pendlerAnimation/view.js
@@ -4,14 +4,8 @@ import "./style.less";
 
 const PendlerAnimationView = Backbone.View.extend({
     events: {
-        "click .start": "start",
-        "click .reset": "reset",
-        "click .csv-download": "createAlertBeforeDownload",
-        "click .btn-remove-features": "removeFeatures",
-        "change #select-kreis": "setKreis",
-        "change #select-gemeinde": "setGemeinde",
-        "change #select-trefferAnzahl": "setTrefferAnzahl",
-        "change input[type=radio]": "setDirection"
+        "change #select-class": "selectClass",
+        "change .select-level": "selectLevel"
     },
 
     initialize: function () {
@@ -19,17 +13,14 @@ const PendlerAnimationView = Backbone.View.extend({
             // ändert sich der Fensterstatus wird neu gezeichnet
             "change:isActive": this.render,
             // ändert sich eins dieser Attribute wird neu gezeichnet
-            "change:gemeinden change:gemeinde change:trefferAnzahl change:direction change:emptyResult change:animating change:pendlerLegend": this.render,
             "render": this.render
         });
     },
     model: new PendlerAnimationModel(),
-    tagName: "form",
-    id: "animation-tool",
+    id: "pendler-animation",
     template: _.template(PendlerAnimationTemplate),
-
     render: function (model, value) {
-        if (value || !model.get("animating")) {
+        if (value) {
             this.setElement(document.getElementsByClassName("win-body")[0]);
             this.$el.html(this.template(model.toJSON()));
             this.delegateEvents();
@@ -39,33 +30,16 @@ const PendlerAnimationView = Backbone.View.extend({
         }
         return this;
     },
-    removeFeatures: function () {
-        this.model.clear();
-    },
-    start: function () {
-        this.model.prepareAnimation();
-    },
-    reset: function () {
-        this.model.stopAnimation();
-    },
-    createAlertBeforeDownload: function () {
-        this.model.createAlertBeforeDownload();
-    },
+    selectClass: function (evt) {
+        const value = evt.target.value;
 
-    setKreis: function (evt) {
-        this.model.setKreis(evt.target.value);
+        this.model.selectClass(value);
     },
+    selectLevel: function (evt) {
+        const value = evt.target.value,
+            level = parseInt(evt.target.attributes.level.value, 10);
 
-    setGemeinde: function (evt) {
-        this.model.setGemeinde(evt.target.value);
-    },
-
-    setTrefferAnzahl: function (evt) {
-        this.model.setTrefferAnzahl(evt.target.value);
-    },
-
-    setDirection: function (evt) {
-        this.model.setDirection(evt.target.value);
+        this.model.selectLevel(level, value);
     }
 });
 
