ad29cb74ae67030173c1b3dc5bb38ac10afffa12
itm.weiter <itm.weiter@muenchen.de>
2021-09-09 15:25:01 +0200

    removed potree test

--- potree/store/gettersPotree.js
+++
@@ -1,8 +0,0 @@
-import {generateSimpleGetters} from ".../../../src/app-store/utils/generators";
-import PotreeState from "./statePotree";
-
-const getters = {
-    ...generateSimpleGetters(PotreeState)
-};
-
-export default getters;
