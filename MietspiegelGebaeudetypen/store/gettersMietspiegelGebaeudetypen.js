b314bcb36b4199c671dcf87df8547790713b612f
Jonas Weiter <jonas.weiter@muenchen.de>
2022-11-11 07:54:11 +0100

    deleted mietspiegel addons in branch master, they moved to mietspiegel

--- MietspiegelGebaeudetypen/store/gettersMietspiegelGebaeudetypen.js
+++
@@ -1,8 +0,0 @@
-import {generateSimpleGetters} from ".../../../src/app-store/utils/generators";
-import stateMietspiegelGebaeudetypen from "./stateMietspiegelGebaeudetypen";
-
-const getters = {
-    ...generateSimpleGetters(stateMietspiegelGebaeudetypen)
-};
-
-export default getters;
--- MietspiegelWohnlage/store/gettersMietspiegelWohnlage.js
+++
@@ -1,8 +0,0 @@
-import {generateSimpleGetters} from ".../../../src/app-store/utils/generators";
-import stateMietspiegelWohnlage from "./stateMietspiegelWohnlage";
-
-const getters = {
-    ...generateSimpleGetters(stateMietspiegelWohnlage)
-};
-
-export default getters;
