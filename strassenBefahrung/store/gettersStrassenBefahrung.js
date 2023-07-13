25c5587ee9c3bb46609c6f3a03297e9f6c0ab4a6
Jonas Weiter <jonas.weiter@muenchen.de>
2023-07-13 10:23:09 +0200

    new brach for wahlraumfinder

--- strassenBefahrung/store/gettersStrassenBefahrung.js
+++
@@ -1,8 +0,0 @@
-import {generateSimpleGetters} from ".../../../src/app-store/utils/generators";
-import StrassenBefahrungState from "./stateStrassenBefahrung";
-
-const getters = {
-    ...generateSimpleGetters(StrassenBefahrungState)
-};
-
-export default getters;
